bdc6701f2b5b28c7bcc43035891d6157fd237354
dmsssss4 <141887669+dmsssss@users.noreply.github.com>
2023-08-10 20:31:28 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import { getMovieList, searchMovie } from "./api"
+import { useEffect, useState } from 'react';
+
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([])
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result)
+    })
+  }, [])
+
+  const PopularMoviesList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className='Movie-wrapper' key={i}>
+          <div className='Movie-title'>{movie.title}</div>
+          <img className='Movie-image' src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
+          <div className='Movie-date'>RELEASE : {movie.release_date}</div>
+          <div className='Movie-rate'>{movie.vote_average}☆</div>
+        </div>
+      )
+    })
+  }
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='teks'>DHIMASMOVIE</h1>
+        <input
+          placeholder='cari film disini....' className='Movie-search'
+          onChange={({ target }) => search(target.value)} />
+        <div className="Movie-container">
+          <PopularMoviesList />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
